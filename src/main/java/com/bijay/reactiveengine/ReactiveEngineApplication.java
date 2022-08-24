04560e2720af6fd0f69f5c6d12b4edf01d56387e
bijaythapaa <dbijaythapa@gmail.com>
2022-08-24 08:40:50 +0545

    initialize project

---
+++ src/main/java/com/bijay/reactiveengine/ReactiveEngineApplication.java
@@ -0,0 +1,13 @@
+package com.bijay.reactiveengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveEngineApplication.class, args);
+	}
+
+}
